dc58656d75451fb33649170ee36d3e7a8411380e
sosboy888 <swastiksharma88@gmail.com>
2018-03-12 21:34:21 +0530

    Initial commit

---
+++ app/src/main/java/com/tictactoe/sosboy888/tictactoegame/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tictactoe.sosboy888.tictactoegame
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
